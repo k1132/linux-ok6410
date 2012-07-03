56f46f736a1998c8e29b231b72a006c4eb8fc461
Du, Changbin <changbin.du@gmail.com>
2012-07-04 03:14:48 +0800

    media: gpio-ir-recv: add allowed_protos and map_name for platform data
    It's better to give platform code a chance to specify the allowed
    protocols and which keymap to use.

--- drivers/media/rc/gpio-ir-recv.c
+++ drivers/media/rc/gpio-ir-recv.c
@@ -83,11 +83,17 @@ static int __devinit gpio_ir_recv_probe(struct platform_device *pdev)
 	}
 
 	rcdev->driver_type = RC_DRIVER_IR_RAW;
-	rcdev->allowed_protos = RC_TYPE_ALL;
 	rcdev->input_name = GPIO_IR_DEVICE_NAME;
 	rcdev->input_id.bustype = BUS_HOST;
 	rcdev->driver_name = GPIO_IR_DRIVER_NAME;
-	rcdev->map_name = RC_MAP_EMPTY;
+	if (pdata->allowed_protos)
+		rcdev->allowed_protos = pdata->allowed_protos;
+	else
+		rcdev->allowed_protos = RC_TYPE_ALL;
+	if (pdata->map_name)
+		rcdev->map_name = pdata->map_name;
+	else
+		rcdev->map_name = RC_MAP_EMPTY;
 
 	gpio_dev->rcdev = rcdev;
 	gpio_dev->gpio_nr = pdata->gpio_nr;
